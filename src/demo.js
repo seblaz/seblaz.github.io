c6410ffc5096742724522e4fee55655f2a546c6b
seba <sebastian.blazquez96@gmail.com>
2020-04-13 21:41:20 -0300

    Agrego prerendering.

---
+++ src/demo.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+
+const app = express();
+
+app.use(express.static('../build'));
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
