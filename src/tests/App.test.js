e698101e17f55a806d4f906cafd45ab9bbe4cdee
seba <sebastian.blazquez96@gmail.com>
2020-04-15 12:39:21 -0300

    Fix test.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,34 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import App from 'app';
 
-test('renders learn react link', () => {
-  const { getAllByText } = render(<App />);
-  const linkElement = getAllByText(/Seblaz/i)[0];
-  expect(linkElement).toBeInTheDocument();
+/**
+ * Mock window.matchMedia.
+ */
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+    })),
+});
+
+/**
+ * Mock window.scrollTo.
+ */
+Object.defineProperty(window, 'scrollTo', {
+  value: () => null,
+  writable: true
+});
+
+test('renders app', () => {
+    const {getAllByText} = render(<App/>);
+    const linkElement = getAllByText(/Seblaz/i)[0];
+    expect(linkElement).toBeInTheDocument();
 });
